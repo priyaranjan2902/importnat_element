d0a6ad99a60330b1ebcb0cdece6c41905bd3d9d1
Priyaranjan kumar <122117243+priyaranjan2902@users.noreply.github.com>
2024-06-28 14:47:03 +0530

    Add files via upload

---
+++ login_signup/script.js
@@ -0,0 +1,53 @@
+(function($) {
+  $(document).ready(function() {
+    $('nav ul li a:not(:only-child)').on('click', function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+
+    $('html').on('click', function() {
+      $('.nav-dropdown').hide();
+    });
+
+    $('#nav-toggle').on('click', function() {
+      $('nav ul').slideToggle();
+      this.classList.toggle('active');
+    });
+  });
+})(jQuery);
+
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+const rollIdPattern = /^b52\d{4}$/;
+
+document.querySelector("#sign-in-form").addEventListener("submit", function(event) {
+  const rollId = document.querySelector("#roll-id-sign-in").value;
+  if (!rollIdPattern.test(rollId)) {
+    alert("Invalid Roll ID. It should be in the format b52XXXX.");
+    event.preventDefault();
+  }
+});
+
+document.querySelector("#sign-up-form").addEventListener("submit", function(event) {
+  const rollId = document.querySelector("#roll-id-sign-up").value;
+  if (!rollIdPattern.test(rollId)) {
+    alert("Invalid Roll ID. It should be in the format b52XXXX.");
+    event.preventDefault();
+  }
+});
+
+
+
+
