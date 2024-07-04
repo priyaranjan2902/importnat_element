bb651a34505c22112282634dc8babbc337f31181
priyaranjan2902 <rajprience2902@gmail.com>
2024-07-04 14:28:25 +0530

    update about

--- About_Page1/script.js
+++ About_Page1/script.js
@@ -85,23 +85,36 @@ function animate() {
 
 animate();
 
+  /*  NAVBAR*/
 
+$(document).ready(function () {
+  $('.menu-btn').click(function () {
+      $('.navbar .menu').toggleClass('active');
+      $('.menu-btn i').toggleClass('active');
+  });
+});
 
-(function($) { 
-    $(function() { 
-      $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        $('.dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
+$(document).ready(function () {
+  $('.menu-btn').click(function () {
+      $('.menu').toggleClass('active');
+  });
+});
+
+(function ($) {
+  $(function () {
+      $('nav ul li a:not(:only-child)').click(function (e) {
+          $(this).siblings('.dropdown').toggle();
+          $('.dropdown').not($(this).siblings()).hide();
+          e.stopPropagation();
       });
-      $('html').click(function() {
-        $('.nav-dropdown').hide();
+      $('html').click(function () {
+          $('.dropdown').hide();
       });
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
+      $('#nav-toggle').click(function () {
+          $('nav ul').slideToggle();
       });
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
+      $('#nav-toggle').on('click', function () {
+          this.classList.toggle('active');
       });
-    }); 
-  })(jQuery);
+  });
+})(jQuery);
--- login_signup/script.js
+++ login_signup/script.js
@@ -1,34 +1,4 @@
-$(document).ready(function () {
-  $('.menu-btn').click(function () {
-      $('.navbar .menu').toggleClass('active');
-      $('.menu-btn i').toggleClass('active');
-  });
-});
 
-$(document).ready(function () {
-  $('.menu-btn').click(function () {
-      $('.menu').toggleClass('active');
-  });
-});
-
-(function ($) {
-  $(function () {
-      $('nav ul li a:not(:only-child)').click(function (e) {
-          $(this).siblings('.dropdown').toggle();
-          $('.dropdown').not($(this).siblings()).hide();
-          e.stopPropagation();
-      });
-      $('html').click(function () {
-          $('.dropdown').hide();
-      });
-      $('#nav-toggle').click(function () {
-          $('nav ul').slideToggle();
-      });
-      $('#nav-toggle').on('click', function () {
-          this.classList.toggle('active');
-      });
-  });
-})(jQuery);
 
 
 const sign_in_btn = document.querySelector("#sign-in-btn");
@@ -60,6 +30,41 @@ document.querySelector("#sign-up-form").addEventListener("submit", function(even
     event.preventDefault();
   }
 });
+
+
+
+
+$(document).ready(function () {
+  $('.menu-btn').click(function () {
+      $('.navbar .menu').toggleClass('active');
+      $('.menu-btn i').toggleClass('active');
+  });
+});
+
+$(document).ready(function () {
+  $('.menu-btn').click(function () {
+      $('.menu').toggleClass('active');
+  });
+});
+
+(function ($) {
+  $(function () {
+      $('nav ul li a:not(:only-child)').click(function (e) {
+          $(this).siblings('.dropdown').toggle();
+          $('.dropdown').not($(this).siblings()).hide();
+          e.stopPropagation();
+      });
+      $('html').click(function () {
+          $('.dropdown').hide();
+      });
+      $('#nav-toggle').click(function () {
+          $('nav ul').slideToggle();
+      });
+      $('#nav-toggle').on('click', function () {
+          this.classList.toggle('active');
+      });
+  });
+})(jQuery);
 
 
 
