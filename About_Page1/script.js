e1e6d6b956b869fadf56b4d22a9858139a973553
priyaranjan2902 <rajprience2902@gmail.com>
2024-07-04 14:51:31 +0530

    add scrollbar

--- About_Page1/script.js
+++ About_Page1/script.js
@@ -1,33 +1,32 @@
-
-  /*  NAVBAR*/
+// ----NAVBAR----//
 $(document).ready(function () {
-  $('.menu-btn').click(function () {
-      $('.navbar .menu').toggleClass('active');
-      $('.menu-btn i').toggleClass('active');
-  });
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
 });
 
 $(document).ready(function () {
-  $('.menu-btn').click(function () {
-      $('.menu').toggleClass('active');
-  });
+    $('.menu-btn').click(function () {
+        $('.menu').toggleClass('active');
+    });
 });
 
 (function ($) {
-  $(function () {
-      $('nav ul li a:not(:only-child)').click(function (e) {
-          $(this).siblings('.dropdown').toggle();
-          $('.dropdown').not($(this).siblings()).hide();
-          e.stopPropagation();
-      });
-      $('html').click(function () {
-          $('.dropdown').hide();
-      });
-      $('#nav-toggle').click(function () {
-          $('nav ul').slideToggle();
-      });
-      $('#nav-toggle').on('click', function () {
-          this.classList.toggle('active');
-      });
-  });
+    $(function () {
+        $('nav ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.dropdown').toggle();
+            $('.dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function () {
+            $('.dropdown').hide();
+        });
+        $('#nav-toggle').click(function () {
+            $('nav ul').slideToggle();
+        });
+        $('#nav-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
+    });
 })(jQuery);
