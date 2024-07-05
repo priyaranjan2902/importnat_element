1d5eeb07e5c9479f3352021388295a0facb4c74d
priyaranjan2902 <rajprience2902@gmail.com>
2024-07-05 07:49:27 +0530

    UPDATE NAVBAR

--- login_signup/script.js
+++ login_signup/script.js
@@ -1,39 +1,3 @@
-
-
-
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
-
-const rollIdPattern = /^b52\d{4}$/;
-
-document.querySelector("#sign-in-form").addEventListener("submit", function(event) {
-  const rollId = document.querySelector("#roll-id-sign-in").value;
-  if (!rollIdPattern.test(rollId)) {
-    alert("Invalid Roll ID. It should be in the format b52XXXX.");
-    event.preventDefault();
-  }
-});
-
-document.querySelector("#sign-up-form").addEventListener("submit", function(event) {
-  const rollId = document.querySelector("#roll-id-sign-up").value;
-  if (!rollIdPattern.test(rollId)) {
-    alert("Invalid Roll ID. It should be in the format b52XXXX.");
-    event.preventDefault();
-  }
-});
-
-
-
-
 $(document).ready(function () {
   $('.menu-btn').click(function () {
       $('.navbar .menu').toggleClass('active');
@@ -65,6 +29,37 @@ $(document).ready(function () {
       });
   });
 })(jQuery);
+
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+const rollIdPattern = /^b52\d{4}$/;
+
+document.querySelector("#sign-in-form").addEventListener("submit", function(event) {
+  const rollId = document.querySelector("#roll-id-sign-in").value;
+  if (!rollIdPattern.test(rollId)) {
+    alert("Invalid Roll ID. It should be in the format b52XXXX.");
+    event.preventDefault();
+  }
+});
+
+document.querySelector("#sign-up-form").addEventListener("submit", function(event) {
+  const rollId = document.querySelector("#roll-id-sign-up").value;
+  if (!rollIdPattern.test(rollId)) {
+    alert("Invalid Roll ID. It should be in the format b52XXXX.");
+    event.preventDefault();
+  }
+});
 
 
 
